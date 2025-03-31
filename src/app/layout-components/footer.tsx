5c2719ffb963ed1fefe95029b4c1880b0c35a355
Aayush Rai <2aayushrai@gmail.com>
2025-03-31 17:07:54 +0545

    Refactor GalleryPage to use Card components for better UI

--- src/app/layout-components/footer.tsx
+++ src/app/layout-components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <main className="fixed bottom-0 w-full">
-      <div className="bg-gray-800 flex justify-center p-4 w-full gap-4">
+      <div className="bg-gray-800 flex justify-center p-2 w-full gap-4">
         <p>About</p>
         <p>Links</p> <p>Contact us</p>
       </div>
